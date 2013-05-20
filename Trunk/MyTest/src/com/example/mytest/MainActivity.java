63ab9d5fda6de314b025c28ed33721ebcaae5bc4
Leroy <leroy.rocks@student.stenden.com>
2013-05-20 13:35:45 +0200

    MyTest project
    Maakt connectie via internet naar server via SOAP.
    Verstuurd data naar server.
    Toont response data.

---
+++ Trunk/MyTest/src/com/example/mytest/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.mytest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	private static final String SOAP_ACTION = "http://tempuri.org/Welkom";
+	private static final String METHOD_NAME = "Welkom";
+	private static final String NAMESPACE = "http://tempuri.org/";
+	private static final String URL = "http://techniek.server-ict.nl:20824/Service.asmx";
+	TextView tv;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		tv = (TextView)findViewById(R.id.TextView1);
+		
+		SoapObject Request = new SoapObject(NAMESPACE, METHOD_NAME);
+		Request.addProperty("naam", "Leroy");
+		
+		SoapSerializationEnvelope soapEnvelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+		soapEnvelope.dotNet = true;
+		soapEnvelope.setOutputSoapObject(Request);
+		
+		HttpTransportSE aht = new HttpTransportSE(URL);
+		try
+		{
+			aht.call(SOAP_ACTION, soapEnvelope);
+			SoapPrimitive resultString = (SoapPrimitive)soapEnvelope.getResponse();
+			tv.setText(resultString.toString());
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+		
+		
+	}
+
+}
