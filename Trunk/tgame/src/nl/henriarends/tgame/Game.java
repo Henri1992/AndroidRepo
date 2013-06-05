e4db35c44fb3ed386021db6a3a06a28404e88d29
Wybren Jongstra <wybren.jongstra@student.stenden.com>
2013-06-05 10:51:35 +0200

    voor omzetten naar object georienteerd

--- Trunk/tgame/src/nl/henriarends/tgame/Game.java
+++ Trunk/tgame/src/nl/henriarends/tgame/Game.java
@@ -12,78 +12,32 @@ import android.content.pm.ActivityInfo;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Game extends Activity {
+
+	int aantalOgen;
 
 	boolean player1 = false;
 	boolean player2 = false;
 	boolean player3 = false;
 	boolean player4 = false;
-	
-	private static final String SOAP_ACTION_R = "http://tempuri.org/Read";
-	private static final String METHOD_NAME_R = "Read";
+
+	private static final String SOAP_ACTION_R = "http://tempuri.org/diceGet";
+	private static final String METHOD_NAME_R = "diceGet";
 	private static final String NAMESPACE_R = "http://tempuri.org/";
 	private static final String URL_R = "http://techniek.server-ict.nl:20824/Service.asmx";
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.game);
-		this.setRequestedOrientation(
-		ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 		new AsyncTaskClass_R().execute(false);
-		
-		player1 = true;
-		
-		if(player1 == true)
-		{
-			ImageView imagePlayer1 = (ImageView) findViewById(R.id.player1);
-			imagePlayer1.setImageResource(R.drawable.player1selected);
-			ImageView imagePlayer2 = (ImageView) findViewById(R.id.player2);
-			imagePlayer2.setImageResource(R.drawable.player2);
-			ImageView imagePlayer3 = (ImageView) findViewById(R.id.player3);
-			imagePlayer3.setImageResource(R.drawable.player3);
-			ImageView imagePlayer4 = (ImageView) findViewById(R.id.player4);
-			imagePlayer4.setImageResource(R.drawable.player4);
-		}
-		else if(player2 == true)
-		{
-			ImageView imagePlayer5 = (ImageView) findViewById(R.id.player1);
-			imagePlayer5.setImageResource(R.drawable.player1);
-			ImageView imagePlayer6 = (ImageView) findViewById(R.id.player2);
-			imagePlayer6.setImageResource(R.drawable.player2selected);
-			ImageView imagePlayer7 = (ImageView) findViewById(R.id.player3);
-			imagePlayer7.setImageResource(R.drawable.player3);
-			ImageView imagePlayer8 = (ImageView) findViewById(R.id.player4);
-			imagePlayer8.setImageResource(R.drawable.player4);
-		}
-		else if(player3 == true)
-		{
-			ImageView imagePlayer9 = (ImageView) findViewById(R.id.player1);
-			imagePlayer9.setImageResource(R.drawable.player1);
-			ImageView imagePlayer10 = (ImageView) findViewById(R.id.player2);
-			imagePlayer10.setImageResource(R.drawable.player2);
-			ImageView imagePlayer11 = (ImageView) findViewById(R.id.player3);
-			imagePlayer11.setImageResource(R.drawable.player3selected);
-			ImageView imagePlayer12= (ImageView) findViewById(R.id.player4);
-			imagePlayer12.setImageResource(R.drawable.player4);
-		}
-		else if(player4 == true)
-		{
-			ImageView imagePlayer13 = (ImageView) findViewById(R.id.player1);
-			imagePlayer13.setImageResource(R.drawable.player1);
-			ImageView imagePlayer14 = (ImageView) findViewById(R.id.player2);
-			imagePlayer14.setImageResource(R.drawable.player2);
-			ImageView imagePlayer15 = (ImageView) findViewById(R.id.player3);
-			imagePlayer15.setImageResource(R.drawable.player3);
-			ImageView imagePlayer16 = (ImageView) findViewById(R.id.player4);
-			imagePlayer16.setImageResource(R.drawable.player4selected);
-		}
+
 	}
-	
+
 	// /////////////////////////////
 	// Inner class AsyncTaskClass_R
 	public class AsyncTaskClass_R extends AsyncTask<Boolean, Void, String> {
@@ -99,7 +53,7 @@ public class Game extends Activity {
 				// een Runnable met new AsyncTaskClass_R().execute();
 				// te doen. Je belast hierdoor niet de main thread.
 				try {
-					Thread.sleep(10000);
+					Thread.sleep(5000);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
@@ -113,10 +67,11 @@ public class Game extends Activity {
 			soapEnvelope.setOutputSoapObject(Request);
 
 			HttpTransportSE aht = new HttpTransportSE(URL_R);
-			
+
 			try {
 				aht.call(SOAP_ACTION_R, soapEnvelope);
-				SoapPrimitive resultString = (SoapPrimitive) soapEnvelope.getResponse();
+				SoapPrimitive resultString = (SoapPrimitive) soapEnvelope
+						.getResponse();
 
 				return resultString.toString();
 			} catch (Exception e) {
@@ -147,7 +102,8 @@ public class Game extends Activity {
 				}
 			//}
 
-			tv_R.setText("Ontvangen " + result.toString());
+			tv_R.setText("Ontvangen " + result);
+			aantalOgen = Integer.parseInt(result);
 		}
 	}
 }
--- Trunk/tgame/src/nl/henriarends/tgame/GameView.java
+++ Trunk/tgame/src/nl/henriarends/tgame/GameView.java
@@ -1,57 +1,12 @@
 package nl.henriarends.tgame;
 
 import android.content.Context;
-import android.graphics.Canvas;
 import android.view.View;
 
 public class GameView extends View{
-	
-	int x;
-	int y;
-	int aantalOgen = 0;
-	boolean player1 = false;
-	
+
 	public GameView(Context context) {
 		super(context);
 		// TODO Auto-generated constructor stub
-		init();
-	}
-	
-	private void init() {
-		// TODO Auto-generated method stub
-		x = 0;
-		y = 0;
-	}
-
-	@Override
-	protected void onDraw(Canvas canvas) {
-		// TODO Auto-generated method stub
-		super.onDraw(canvas);
-		player1 = true;
-		aantalOgen = 6;
-		
-		if (player1 = true)
-		{
-			switch(aantalOgen)
-			{
-				case 1: // 1 opzij of 1 omhoog of 1 omlaag
-						// welke pion
-						// locatie oude pion (x,y)
-						// pion verplaatsen (x,y)
-					break;
-				case 2: // 2 opzij of 2 omhoog of 2 omlaag of 1 opzij en 1 omhoog of 1 opzij en 1 omlaag
-					break;
-				case 3: // 3 opzij of 3 omhoog of 3 omlaag of 1 opzij en 2 omhoog of 1 opzij en 2 omlaag 
-					// 2 opzij en 1 omhoog of 2 opzij en 1 omlaag
-					break;
-				case 4:
-					break;
-				case 5:
-					break;
-				case 6:
-					break;
-			}
-		}
-	}
-	
+	}	
 }
