2ce486d09a18d4907d0d0551c2c64f6b035895e7
Jordi Sipkens <jordisipkens@gmail.com>
2013-06-09 17:06:23 +0200

    HowtoplayLos
    De how to play in een los project proberen te maken op aanraden van
    jeroen. Helaas nog steeds een onbekende error waardoor die hem bij
    opstarten afsluit. Maar weer langs Jeroen.

---
+++ Trunk/HowToPlayLos/src/com/example/howtoplaylos/ThirdFragment.java
@@ -0,0 +1,16 @@
+package com.example.howtoplaylos;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ThirdFragment {
+
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// Inflating layout
+		View v = inflater.inflate(R.layout.third_fragment, container, false);
+		return v;
+	}
+}
